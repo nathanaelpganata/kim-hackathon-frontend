cdb8345115c2490a1fa07bd0579e158aee3ff231
naufalbasara <naufalbasara@outlook.com>
2023-09-23 17:29:05 +0700

    feat: add admin view

--- app/admin/orders/data/page.tsx
+++ app/admin/orders/data/page.tsx
@@ -7,20 +7,19 @@ import React from 'react';
 
 import DashboardLayout from '@/components/layouts/dashboard/DashboardLayout';
 import { DataTable } from '@/components/table/DataTable';
-// import { LuMoreHorizontal } from 'react-icons/lu';
-// import { DataTableColumnHeader } from '@/components/table/DataTableColumnHeader';
-// import { Badge } from '@/components/ui/badge';
-// import { Button } from '@/components/ui/button';
+import { LuMoreHorizontal } from 'react-icons/lu';
+import { Button } from '@/components/ui/button';
 import { Checkbox } from '@/components/ui/checkbox';
-// import {
-//   DropdownMenu,
-//   DropdownMenuContent,
-//   DropdownMenuItem,
-//   DropdownMenuLabel,
-//   DropdownMenuSeparator,
-//   DropdownMenuTrigger,
-// } from '@/components/ui/dropdown-menu';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 
+import { useRouter } from 'next/navigation';
 
 // Data Definition
 type OrderSchemaData = {
@@ -28,80 +27,105 @@ type OrderSchemaData = {
 }
 
 type OrderSchema = {
-
+  id: string;
   no_pemesanan: string;
-  customer_name: string;
-  customer_email: string;
-  customer_phone: string;
+  customer: any;
   quantity: number;
   category: string;
-
-
 };
 
-// Columns Definition
-const columns: ColumnDef<OrderSchema>[] = [
-  {
-    id: 'select',
-    header: ({ table }) => (
-      <Checkbox
-        checked={table.getIsAllPageRowsSelected()}
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label='Select all'
-        className='translate-y-[2px]'
-      />
-    ),
-    cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label='Select row'
-        className='translate-y-[2px]'
-      />
-    ),
-    enableSorting: false,
-    enableHiding: false,
-  },
-  {
-    id: 'no_pemesananan',
-    header: 'No Pemesanan',
-    accessorKey: 'no_pemesanan',
-  },
-  {
-    id: 'customer_name',
-    header: 'Nama Customer',
-    accessorKey: 'id',
-  },
-  {
-    id: 'customer_email',
-    header: 'Email Customer',
-    accessorKey: 'customer_email',
-  },
-  {
-    id: 'customer_phone',
-    header: 'Phone Customer',
-    accessorKey: 'customer_phone',
-  },
-  {
-    id: 'category',
-    header: 'Kategori',
-    accessorKey: 'category',
-  },
-];
-
-
-
 const OrderTablePage = () => {
+  const router = useRouter()
   const { data: OrderData, isLoading } = useQuery<OrderSchemaData>(['/order'])
+  const columns: ColumnDef<OrderSchema>[] = [
+    {
+      id: 'select',
+      header: ({ table }) => (
+        <Checkbox
+          checked={table.getIsAllPageRowsSelected()}
+          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+          aria-label='Select all'
+          className='translate-y-[2px]'
+        />
+      ),
+      cell: ({ row }) => (
+        <Checkbox
+          checked={row.getIsSelected()}
+          onCheckedChange={(value) => row.toggleSelected(!!value)}
+          aria-label='Select row'
+          className='translate-y-[2px]'
+        />
+      ),
+      enableSorting: false,
+      enableHiding: false,
+    },
+    {
+      id: 'id',
+      header: 'ID',
+      accessorKey: 'id',
+    },
+    {
+      id: 'no_pemesananan',
+      header: 'No Pemesanan',
+      accessorKey: 'no_pemesanan',
+    },
+    {
+      id: 'customer_name',
+      header: 'Nama Customer',
+      accessorKey: 'customer_name',
+    },
+  
+    {
+      id: 'customer_email',
+      header: 'Email Customer',
+      accessorKey: 'customer_email',
+    },
+    {
+      id: 'customer_phone',
+      header: 'Phone Customer',
+      accessorKey: 'customer_phone',
+    },
+    {
+      id: 'category',
+      header: 'Kategori',
+      accessorKey: 'category',
+    },
+    {
+      id: 'actions',
+      header: 'Actions',
+      cell: ({ row }) => {
+        const todo = row.original;
+        return (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant='ghost' className='h-8 w-8 p-0'>
+                <span className='sr-only'>Open menu</span>
+                <LuMoreHorizontal className='h-4 w-4' />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align='end'>
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem
+              onClick={async () => {
+                router.push(`${todo.id}`)
+              }}
+              >Lihat detail pesanan</DropdownMenuItem>
+              <DropdownMenuItem className='bg-[#2CA87F]'>Terima pesanan</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        );
+      },
+    },
+  ];
 
-  console.log(OrderData)
-
-  const mappedOrderData = OrderData?.data.map((order) => {
+  const mappedOrderData = OrderData?.data.map((order: OrderSchema) => {
     return {
+      id: order.id,
       no_pemesanan: order.no_pemesanan,
-      customer_name: order.customer_name,
-      customer_email: order.customer_email,
-      customer_phone: order.customer_phone,
+      customer_name: order.customer[0].customer_name,
+      customer_email: order.customer[0].customer_email,
+      customer_phone: order.customer[0].customer_phone,
       category: order.category,
     }
   })
@@ -109,14 +133,19 @@ const OrderTablePage = () => {
 
 
   return <DashboardLayout>
-    {!isLoading ? (
+    <div className='flex flex-col items-left justify-center pt-10 px-8 mx-8'>
+      <h1 className='text-5xl font-bold'>Pesanan</h1>
+      <div className='mt-12'>
+      {!isLoading ? (
       <DataTable
-        data={mappedOrderData as OrderSchema[]}
+        data={mappedOrderData as any}
         columns={columns}
       />
     ) : (
       <>Loading...</>
     )}
+      </div>
+    </div>
 
   </DashboardLayout>;
 };
--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['localhost', 'i.imgur.com', 'imgur.com', 'hackthon.fly.dev'], // Add 'imgur.com' here
+    },
+  };
+  
+  module.exports = nextConfig;
